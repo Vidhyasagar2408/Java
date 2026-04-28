b6199662a424a6b9c8c339cf8916d5b02622a309
vidhyasagar <vs1522250@gmail.com>
2026-04-28 09:20:47 +0530

    OOP

--- object/src/com/object/PersonApplication.java
+++ object/src/com/object/PersonApplication.java
@@ -3,6 +3,7 @@ package com.object;
 public class PersonApplication {
     public static void main(String[] args) {
         Person p1 = new Person();
+        Person p2 = new Person();
         p1.age=12;
         p1.name="Raja";
         System.out.println(p1.age);
@@ -10,5 +11,15 @@ public class PersonApplication {
 
         p1.work();
         p1.sleep();
+
+        System.out.println();
+
+        p2.age=20;
+        p2.name="Rani";
+        System.out.println(p2.age);
+        System.out.println(p2.name);
+
+        p2.work();
+        p2.sleep();
     }
 }
