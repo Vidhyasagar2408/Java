e1bf630f49e536ec80d1b05466e4aa14ab4c62b9
vidhyasagar <vs1522250@gmail.com>
2026-05-27 15:55:35 +0530

    sort And search for key

---
+++ Binary-Search/src/sortAndSearchKey.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sortAndSearchKey {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+    public static void binarySearch(int[] arr, int start, int end){
+
+    }
+    public static void isSorted(int[] arr){
+
+    }
+}
