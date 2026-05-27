6bbcf14d7b89b71f6f48f4b7397aee7fcfd4bf4c
vidhyasagar <vs1522250@gmail.com>
2026-05-27 16:22:16 +0530

    sort And search for key

--- Binary-Search/src/sortAndSearchKey.java
+++ Binary-Search/src/sortAndSearchKey.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class sortAndSearchKey {
@@ -12,13 +13,32 @@ public class sortAndSearchKey {
         }
         System.out.println("Enter the key to be searched: ");
         int key = sc.nextInt();
-
+        boolean sortResult = isSorted(arr);
+        if (sortResult) {
+            int index = binarySearch(arr, key);
+            if (index == -1){
+                System.out.println("Key not found");
+            } else {
+                System.out.println("key found at index: " + index);
+            }
+        } else {
+            System.out.println("Array is not sorted. We need to first sort the array and then find the key.");
+            System.out.println("Sorting the Array...");
+            bubbleSort(arr);
+            System.out.println("Sorted Array: " + Arrays.toString(arr));
+            int index = binarySearch(arr, key);
+            if (index == -1) {
+                System.out.println("Key not found");
+            } else {
+                System.out.println("Key found at index: " + index);
+            }
+        }
     }
     public static int binarySearch(int[] arr, int key){
         int left = 0;
         int right = arr.length - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (key == arr[mid]){
                 return mid;
             } else if (key < arr[mid]) {
@@ -29,13 +49,14 @@ public class sortAndSearchKey {
         }
         return -1;
     }
-    public static void isSorted(int[] arr){
+    public static boolean isSorted(int[] arr){
         int n = arr.length;
         for (int i = 0; i < n-1; i++) {
             if (arr[i] > arr[i+1]) {
-                bubbleSort(arr);
+                return false;
             }
         }
+        return true;
     }
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
