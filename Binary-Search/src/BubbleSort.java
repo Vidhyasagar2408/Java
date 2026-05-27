06453033da55cc2978704e6ab91e81423451a263
vidhyasagar <vs1522250@gmail.com>
2026-05-27 15:46:24 +0530

    bubbleSort

---
+++ Binary-Search/src/BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before sorting: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
+    }
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
