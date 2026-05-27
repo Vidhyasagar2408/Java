8faf41982eb40a5c03bbcd0e0f620c7284e92314
vidhyasagar <vs1522250@gmail.com>
2026-05-27 16:07:05 +0530

    sort And search for key

--- Binary-Search/src/sortAndSearchKey.java
+++ Binary-Search/src/sortAndSearchKey.java
@@ -14,13 +14,30 @@ public class sortAndSearchKey {
         int key = sc.nextInt();
 
     }
-    public static void binarySearch(int[] arr, int start, int end){
-
+    public static int binarySearch(int[] arr, int key){
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (key == arr[mid]){
+                return mid;
+            } else if (key < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
     public static void isSorted(int[] arr){
-
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                bubbleSort(arr);
+            }
+        }
     }
-    public static void bibbleSort(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n-1; i++) {
             for (int j = 0; j < n-1-i; j++) {
