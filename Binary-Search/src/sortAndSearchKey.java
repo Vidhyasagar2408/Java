80b2c70d025e6c9f51e6466ed9dfb5472e51e209
vidhyasagar <vs1522250@gmail.com>
2026-05-27 16:00:22 +0530

    sort And search for key

--- Binary-Search/src/sortAndSearchKey.java
+++ Binary-Search/src/sortAndSearchKey.java
@@ -10,11 +10,26 @@ public class sortAndSearchKey {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
+        System.out.println("Enter the key to be searched: ");
+        int key = sc.nextInt();
+
     }
     public static void binarySearch(int[] arr, int start, int end){
 
     }
     public static void isSorted(int[] arr){
 
+    }
+    public static void bibbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
     }
 }
