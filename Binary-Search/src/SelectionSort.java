a34646452249f69911508f5191b2dde8341322b1
vidhyasagar <vs1522250@gmail.com>
2026-05-30 18:37:39 +0530

    Selection sort

--- Binary-Search/src/SelectionSort.java
+++ Binary-Search/src/SelectionSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SelectionSort {
@@ -11,14 +12,10 @@ public class SelectionSort {
             arr[i] = sc.nextInt();
         }
         System.out.println("Array before sorting: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(Arrays.toString(arr));
         System.out.println("Array after sorting: ");
         selectionSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(Arrays.toString(arr));
     }
     static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
