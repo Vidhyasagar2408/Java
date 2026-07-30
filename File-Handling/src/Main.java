89cff64110ddec214c02045bb722955b0848e746
vidhyasagar <vs1522250@gmail.com>
2026-07-30 12:11:47 +0530

    File Handling

---
+++ File-Handling/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path1 = "C:\\FileHandler\\input.txt";
+        String path2 = "C:\\FileHandler\\output.txt";
+
+        FileInputStream fis = new FileInputStream(path1);
+        FileOutputStream fos = new FileOutputStream(path2);
+
+        int data;
+
+        while ((data = fis.read()) != -1) {
+            fos.write(data);
+        }
+
+        fis.close();
+        fos.close();
+    }
+}
