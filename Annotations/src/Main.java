2ac5fd8cb8eb277a3ff8c6f4094cda2f9a53d0ae
vidhyasagar <vs1522250@gmail.com>
2026-08-01 16:19:23 +0530

    Serialization

---
+++ Annotations/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+@SuppressWarnings({"unused","rawtypes","unchecked"})
+public class Main {
+    void  disp1() {
+        System.out.println("disp1");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = a+b+c+d;
+        System.out.println(e);
+    }
+    void decArray() {
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+
+        int a = 10;
+    }
+}
