d9470d320f4bcda61b42c8978dcde9ebd844fcdb
vidhyasagar <vs1522250@gmail.com>
2026-08-01 16:33:07 +0530

    Annotations

--- Annotations/src/CustomAnnotations.java
+++ Annotations/src/CustomAnnotations.java
@@ -1,6 +1,12 @@
 public class CustomAnnotations {
+
     @MinAge(age = 18)
     void display(int age) {
+        System.out.println(age);
+    }
+
+    @MaxAge(age = 60)
+    void display2(int age) {
         System.out.println(age);
     }
 }
--- Annotations/src/MaxAge.java
+++ Annotations/src/MaxAge.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(METHOD)
 
 public @interface MaxAge {
+
+    int age();
+
 }
