c193fea997105fcc1ced1daa6bbaed80a0d8dc3e
vidhyasagar <vs1522250@gmail.com>
2026-08-01 16:39:41 +0530

    Annotations

--- Annotations/src/CustomAnnotations.java
+++ Annotations/src/CustomAnnotations.java
@@ -2,7 +2,7 @@ public class CustomAnnotations {
 
     @MaxAge(age = 60)
     @MinAge(age = 18)
-    void display(int age) {
+    void display(@OneParameter(par = 1) int age) {
         System.out.println(age);
     }
 }
